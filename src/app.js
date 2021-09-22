2ce3c976300d13bb330339381d99d7dd70954bfa
prekshita <prekshitavpatil@gmail.com>
2021-09-22 09:51:36 +0530

    Updated

---
+++ src/app.js
@@ -0,0 +1,160 @@
+//Progression 1:
+//We've got some basic info about Karen's home
+//Debug the type of data provided
+//Return the types concatenated in a single variable
+function moreAboutHome(address,distanceFromTown,hasNeighbours)
+{
+    let createManager = {
+        Address: address,
+        distance: distanceFromTown,
+        Neighbours: hasNeighbours
+    }
+
+    types = typeof(createManager['Address']) + typeof(createManager['distance']) + typeof(createManager['Neighbours'])
+    return types
+
+}
+
+
+//Progression 2:
+//Check if the data given is of the right type
+//parents = String, noOfSiblings = Number, isNuclearFamily = Boolean
+
+function moreAboutKaren(parents,noOfSiblings,isNuclearFamily)
+{
+    let moreAboutKaren =
+    {
+        parents: parents,
+        noofSiblings: noOfSiblings,
+        nuclear: isNuclearFamily,
+    }
+    if (typeof moreAboutKaren['parents'] === typeof "" && typeof moreAboutKaren['noofSiblings'] === 'number' && typeof moreAboutKaren['nuclear'] === 'boolean')
+    {
+        return true
+    }
+    else {
+        return false      
+    } 
+}
+
+
+
+
+//Progression 3:
+//Lily is suspicious about Karen's new friend
+//Karen tells her friend's age and even writes it down
+//Check which one those is not a number (NaN) and return that value
+function doesFriendExist(ageInText,ageInNumber)
+{
+
+    if (ageInText === 'NaN'  ||  ageInNumber === 'NaN')
+    {
+        return  ageInText
+    }
+    if (isNaN(ageInText) || isNaN(ageInNumber))
+    {
+        return NaN    
+    }
+}
+
+
+//Progression 4:
+//Lily gave Karen x sweets
+//Karen ate y sweets herself
+//On her way to the river, she ate another z sweets every n meters travelled
+//Her friend divided the remaining sweets into 2 parts for each
+//How many sweets did her friend get to eat?
+
+function sweetTooth(totalNoOfSweets,sweetsConsumedByKaren,sweetsConsumedInNMeters, metersToTravel)
+{
+    if (isNaN(totalNoOfSweets) || isNaN(sweetsConsumedByKaren) || isNaN(sweetsConsumedInNMeters) || isNaN(metersToTravel))
+    {
+        return "No sweets for Karen's friend"
+    }
+if (totalNoOfSweets === 0 && sweetsConsumedInNMeters === 0 &&  sweetsConsumedByKaren === 0 && metersToTravel === 0)
+    {
+        return totalNoOfSweets;
+    }
+if (sweetsConsumedInNMeters < metersToTravel)
+    {
+        return 0
+    }
+}
+
+
+
+//Progression 5:
+//As Lily moves closer, it gets colder. She checks the temperature on her mobile
+//It only shows in fahrenheit. Convert the data to celsius and return it.
+
+function convertToCelsius(fahrenheit)
+{
+    if (typeof fahrenheit === 'undefined' || typeof fahrenheit === 'string' || typeof fahrenheit === 'object')
+    {
+        return 'Technical Error!'
+    }
+    else
+    {
+        return (5 / 9 * (fahrenheit - 32))
+    }
+}
+
+
+
+//Progression 6:
+//Lily can now do multiple things to deal with this
+//1. Take her daughter to a doctor
+//2. Talk to her husband about it
+//3. Counsel her daughter herself
+//4. Lock her daughter in her room
+//Given a value, return which of these above actions Lily would take
+
+function aDifficultChoice(choice)
+{
+    if (choice === 1)
+    {
+        return 'Take her daughter to a doctor'
+    }
+    else if(choice ===2)
+
+    {
+        return "Talk to her husband about it"
+    }
+    
+    else if(choice===3)
+    
+    {
+        return "Counsel her daughter herself"
+    }
+    
+    else if(choice===4)
+    
+    {
+        return "Lock her daughter in her room"
+    }
+    
+    else if (choice === -1 )
+    {
+        return 'Break down and give up all hope'
+    }
+    else if (typeof choice === 'undefined')
+    
+    {
+        return "Wasn't able to decide"
+    }
+    else
+    {
+        return 'Refused to do anything for Karen'
+    }
+}
+
+
+
+//Progression 7:
+//Lily realized that she'd hurt her daughter
+//All she wants now is for her to stop crying
+//She refuses to talk to her but Lily doesn't stop trying
+//She tries out multiple things hoping for the best
+//Take all of Lily's strategies and concatenate them to a single var
+//Seperate the strategies by a single space
+//Return the length of the complete strategy
